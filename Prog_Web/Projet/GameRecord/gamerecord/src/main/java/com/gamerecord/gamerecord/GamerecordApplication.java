d49d1c1026e9a166f8b32d10da2f2b7339164036
Shooterah <iflox974@gmail.com>
2022-09-28 09:17:28 +0200

    Debut Projet Dev Web - création

---
+++ Prog_Web/Projet/GameRecord/gamerecord/src/main/java/com/gamerecord/gamerecord/GamerecordApplication.java
@@ -0,0 +1,13 @@
+package com.gamerecord.gamerecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamerecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamerecordApplication.class, args);
+	}
+
+}
---
+++ Prog_Web/Projet/GameRecord/gamerecord/src/test/java/com/gamerecord/gamerecord/GamerecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamerecord.gamerecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamerecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
