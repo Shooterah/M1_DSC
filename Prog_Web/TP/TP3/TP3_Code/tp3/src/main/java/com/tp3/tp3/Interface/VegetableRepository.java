99e615a9fd5cf5c40f6ba5aeed6039ebe159ef17
Shooterah <iflox974@gmail.com>
2022-10-22 09:47:03 +0200

    Debut TP VueJs mais pas fin CAR YA PAS LE COURS

---
+++ Prog_Web/TP/TP3/TP3_Code/tp3/src/main/java/com/tp3/tp3/Interface/VegetableRepository.java
@@ -0,0 +1,8 @@
+package com.tp3.tp3.Interface;
+
+import org.springframework.data.repository.CrudRepository;
+import com.tp3.tp3.Class.Vegetable;
+
+public interface VegetableRepository extends CrudRepository<Vegetable, Long> {
+
+}
