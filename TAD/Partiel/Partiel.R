1300196c4482ff0a350ebf0e398c963465a43944
Finalor <iflox974@gmail.com>
2023-03-10 15:53:43 +0100

    Partiel R

---
+++ TAD/Partiel/Partiel.R
@@ -0,0 +1,11 @@
+
+############################################################
+################ CORRECTION DU PARTIEL DE R ################
+############################################################
+
+# Question 1 : Affichez le résumé des informations sur le jeu de données tourbillons. Affichez un histogramme des valeurs pour chaque variable avec la fonction qplot du package ggplot2. Testez avec différentes valeurs du paramètre binwidth si nécessaire pour plus de précision dans le graphique. Est-ce que les variables semblent suivre une distribution normale ? Pourquoi ?
+
+# Réponse 1 :
+
+
+
